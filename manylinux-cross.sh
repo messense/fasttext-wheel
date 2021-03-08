8ff81929a4729e46de53acb64de72c774c5076b3
messense <messense@icloud.com>
2021-03-08 21:53:42 +0800

    Try gcc 10.2.0 based manylinux image

--- manylinux-cross.sh
+++ manylinux-cross.sh
@@ -4,32 +4,30 @@ set -e -x
 sed -i "s/name='fasttext',/name='fasttext-wheel',/" fastText/setup.py
 cd fastText
 
-SYSROOT=`$TARGET_CC --print-sysroot`
-
 # Compile wheels
 python3.6 -m pip install crossenv
-python3.6 -m crossenv /opt/python/cp36-cp36m/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $SYSROOT venv-py36
+python3.6 -m crossenv /opt/python/cp36-cp36m/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $TARGET_SYSROOT venv-py36
 . venv-py36/bin/activate
 pip install wheel
 python setup.py bdist_wheel --plat-name "manylinux2014_$ARCH" --dist-dir ../dist/
 deactivate
 
 python3.7 -m pip install crossenv
-python3.7 -m crossenv /opt/python/cp37-cp37m/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $SYSROOT venv-py37
+python3.7 -m crossenv /opt/python/cp37-cp37m/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $TARGET_SYSROOT venv-py37
 . venv-py37/bin/activate
 pip install wheel
 python setup.py bdist_wheel --plat-name "manylinux2014_$ARCH" --dist-dir ../dist/
 deactivate
 
 python3.8 -m pip install crossenv
-python3.8 -m crossenv /opt/python/cp38-cp38/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $SYSROOT venv-py38
+python3.8 -m crossenv /opt/python/cp38-cp38/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $TARGET_SYSROOT venv-py38
 . venv-py38/bin/activate
 pip install wheel
 python setup.py bdist_wheel --plat-name "manylinux2014_$ARCH" --dist-dir ../dist/
 deactivate
 
 python3.9 -m pip install crossenv
-python3.9 -m crossenv /opt/python/cp39-cp39/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $SYSROOT venv-py39
+python3.9 -m crossenv /opt/python/cp39-cp39/bin/python3 --cc $TARGET_CC --cxx $TARGET_CXX --sysroot $TARGET_SYSROOT venv-py39
 . venv-py39/bin/activate
 pip install wheel
 python setup.py bdist_wheel --plat-name "manylinux2014_$ARCH" --dist-dir ../dist/
