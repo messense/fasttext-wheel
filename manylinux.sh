6f49713f1d6e850443042f3cbd8817c60f0f38af
messense <messense@icloud.com>
2024-02-17 11:25:50 +0800

    Stop building for Python 3.7

--- manylinux.sh
+++ manylinux.sh
@@ -11,7 +11,7 @@ mkdir -p /io/dist/
 mv dist/*.tar.gz /io/dist/
 
 # Compile wheels
-for PYBIN in /opt/python/cp3{7..12}*/bin; do
+for PYBIN in /opt/python/cp3{8..12}*/bin; do
     "${PYBIN}/python" -m pip install build
     "${PYBIN}/python" -m build --wheel -o /io/dist/
 done
